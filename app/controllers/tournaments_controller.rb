1d9c52bf6fc80b7a69199a44b0d289dcf28a18d2
dalf <dalfgan77@gmail.com>
2017-07-12 23:00:29 +0900

    show ranking for a tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,6 +7,7 @@ class TournamentsController < ApplicationController
     @coming_games = @tournament.games.next_5_games.joins("LEFT JOIN guesses ON games.id = guesses.game_id and guesses.member_id = #{current_user.member_id_for_league(@league.id)} and guesses.league_id = #{@league.id}")
     @last_games   = @tournament.games.last_5_games
     @guess        = Guess.new(:league => @league)
+    @rankings     = Ranking.where(:league_id => @league.id, :tournament_id => @tournament.id)
   end
 
 
