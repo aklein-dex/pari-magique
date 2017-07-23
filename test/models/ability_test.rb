18cbd368f675b9605541dbe662ca37061e937f26
dalf <dalfgan77@gmail.com>
2017-07-23 18:55:18 +0900

    add more tests

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -24,4 +24,15 @@ class AbilityTest < ActiveSupport::TestCase
     assert ability.cannot?(:read, League.new)
   end
 
+  test "coach can create/update a request" do
+    # user 1 is a coach of league 1
+    user = users(:user1)
+    other = users(:user2)
+    league = leagues(:league1)
+    ability = Ability.new(user)
+    assert ability.can?(:create,  Request.new(:user => user))
+    assert ability.can?(:update,  Request.new(:user => other, :league => league))
+    # a coach can't delete a request, just reject it
+    assert ability.cannot?(:destroy, Request.new(:user => other, :league => league))
+  end
 end
