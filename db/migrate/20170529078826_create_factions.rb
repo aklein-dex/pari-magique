6d6cb2fac9576f0fd20cc862877eac9848b2191b
dalf <dalfgan77@gmail.com>
2018-06-04 20:26:07 +0900

    fix wrong limit on name of faction

--- db/migrate/20170529078826_create_factions.rb
+++ db/migrate/20170529078826_create_factions.rb
@@ -1,7 +1,7 @@
 class CreateFactions < ActiveRecord::Migration[5.1]
   def change
     create_table :factions do |t|
-      t.string :name, null: false, index: true, limit: 5
+      t.string :name, null: false, index: true, limit: 20
 
       t.timestamps
     end
