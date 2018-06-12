d1f912b0e2bb7b848d70b9c8fceece5979141c24
dalf <dalfgan77@gmail.com>
2018-06-12 22:08:58 +0900

    forgot to add authorization in previous commit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  load_and_authorize_resource
+  
   def index
     @users = User.all
   end
