c5ec339ee7308751d7f4b505e669a6b75fa93de7
dalf <dalfgan77@gmail.com>
2018-06-18 21:50:54 +0900

    remove limit because it is not working correctly

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,6 +1,6 @@
 # One chat room is created for each faction.
 class ChatRoom < ApplicationRecord
   belongs_to :faction
-  
-  has_many :messages, -> { order('created_at asc').limit(30) }, dependent: :destroy
+
+  has_many :messages, -> { order('created_at asc') }, dependent: :destroy
 end
