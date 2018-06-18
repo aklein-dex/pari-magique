4f286d68d8772280d71778424ddb5736fd4184b0
Alex <alexandre.klein@tanukisoftware.com>
2018-06-18 18:02:38 +0900

    make sure to have the chat room messages in order and only the last 30 messages

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,5 +2,5 @@
 class ChatRoom < ApplicationRecord
   belongs_to :faction
   
-  has_many :messages, dependent: :destroy
+  has_many :messages, -> { order('created_at asc').limit(30) }, dependent: :destroy
 end
